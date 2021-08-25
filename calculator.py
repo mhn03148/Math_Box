573ba81b6ed98cb62556b7b6e6eacb4be213ee9d
jinhyoung <mhn03148@naver.com>
2021-08-25 14:53:35 +0900

    change divide function as premium

--- calculator.py
+++ calculator.py
@@ -5,5 +5,13 @@ def add(a,b):
 def substract(a, b):
 	return a-b
 
+<<<<<<< HEAD
 def divide(a, b):
 	return a/b
+=======
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+>>>>>>> main
